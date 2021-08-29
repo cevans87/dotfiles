e6d838ca5cf349a45220d03affa08de25de6f913
Cameron Evans <c.d.evans87@gmail.com>
2021-08-29 14:24:37 -0700

    Add post_install ssh and dconf

---
+++ post_install/barovia/dconf_gnome_terminal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+echo ">>> Dconf gnome terminal"
+# Settings generated with `dconf dump /org/gnome/terminal`
+echo "$(cat <<-EOM
+[legacy]
+theme-variant='dark'
+
+[legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9]
+audible-bell=false
+background-color='rgb(23,20,33)'
+bold-is-bright=true
+foreground-color='rgb(208,207,204)'
+use-theme-colors=false
+EOM
+)" | tee >(dconf load /org/gnome/terminal/)
+echo "<<< Dconf gnome terminal"
--- post_install/default/ssh_add.sh
+++ post_install/default/ssh_add.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo ">>> Ssh add"
 if [ -z "$(ssh-add -L | grep "$(cat $HOME/.ssh/id_ed25519.pub)")" ] ; then
     ssh-add -v $HOME/.ssh/id_ed25519
 fi
+echo "<<< Ssh add"
