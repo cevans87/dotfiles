8b8619b96dc3168b9b21defbefdb3a20ac3dae9c
Cameron Evans <c.d.evans87@gmail.com>
2023-04-08 18:48:19 -0700

    Fixed checking for empty directory in 10.symlink.

--- 03.pip/install.sh
+++ 03.pip/install.sh
@@ -5,10 +5,8 @@ set -e
 BD=$(dirname $(realpath $0))
 DS=( $BD/default $BD/$(whoami) $BD/$(hostname) )
 
-echo ">>> Pip"
 for D in ${DS[@]} ; do
     if [ -d $D ] && [ ! -z "$(ls -A $D)" ] ; then
         python3 -m pip install --user $(eval echo $(cat $D/*))
     fi
 done
-echo "<<< Pip"
--- 10.symlink/install.sh
+++ 10.symlink/install.sh
@@ -7,7 +7,7 @@ DS=( $BD/default $BD/$(whoami) $BD/$(hostname) )
 
 echo ">>> Symlink"
 for D in ${DS[@]} ; do
-    if [ -d $D ] ; then
+    if [ -d $D ] && [ ! -z "$(ls -A $D)" ] ; then
         for RF in $(find $D -type l,f | xargs realpath --no-symlinks --relative-to=$D) ; do
             sudo mkdir -pv $(dirname /$RF)
             sudo ln -fsv $D/$RF /$RF
